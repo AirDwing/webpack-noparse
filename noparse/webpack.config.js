3d26d548da7d9e4d2d8ec63b2174347028d3c24a
willin <willin@willin.org>
2017-06-16 13:48:30 +0800

    add noparse

--- noparse/webpack.config.js
+++ noparse/webpack.config.js
@@ -7,6 +7,11 @@ module.exports = {
     path: path.join(__dirname, '/dist'),
     filename: 'bundle.js'
   },
+  resolve: {
+    alias: {
+      jquery: path.join(__dirname, '../node_modules/jquery/dist/jquery.min.js')
+    }
+  },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
